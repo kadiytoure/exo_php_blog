a9b5853758777aed17e149539a810d91a31a9674
toure <tourekadiy@gmail.com>
2018-08-18 17:31:05 +0200

    form to display content;

--- index.php
+++ index.php
@@ -8,7 +8,16 @@
     <title>Document</title>
 </head>
 <body>
-    
+    <form action="comments.php" method="post">
+    <label for="author">Title:</label>
+    <input type="text" id="idauthor" name="author" value= "<?php
+    if(isset($_COOKIE['author'])){
+         echo htmlspecialchars($_COOKIE['author']);
+    }?>"><br>
+    <label for="content">Content:</label>
+    <input type="text" id="idcontent" name="content" placeholder="enter your message"/><br/>
+    <input type="submit" value="send" /><br/>
+    </form>
 </body>
 </html>
 <?php
